978b4802ed26ee99d24712f61a5c70f52f998dab
AbuQamar Shaikh <shaikhqamar7379@gmail.com>
2024-03-24 02:57:41 +0530

    first commit

--- src/config/config.js
+++ src/config/config.js
@@ -11,8 +11,6 @@ dotenv.config({ path: path.join(__dirname, "../../.env") });
  * @property {number} port - Port number for the server.
  * @property {string} mongoUri - MongoDB connection URI.
  * @property {string} githubApi - GitHub API base URL.
- * @property {string} token - GitHub API authentication token.
- * @property {string} githubUsername - GitHub username.
  */
 
 /**
@@ -23,6 +21,4 @@ module.exports = {
   port: envVars.PORT,
   mongoUri: envVars.MONGO_URI,
   githubApi: envVars.GithubApi,
-  token: envVars.githubAuthToken,
-  githubUsername: envVars.githubUsername,
 };
